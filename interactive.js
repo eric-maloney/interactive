22b499114d68403333d3e2fbae21a02f39589eaf
Eric M. Maloney <eric.maloney@centre.edu>
2020-02-27 15:42:31 -0500

    lab7

---
+++ interactive.js
@@ -0,0 +1,25 @@
+
+d3.select("#george")
+      .on("mouseover", function()
+      {
+         d3.select(this)
+         .attr("src","george washington deleware.jpg")
+    })
+    .on("mouseleave",function()
+       {
+        d3.select(this)
+        .attr("src","george washington portrait.jpg")
+    })
+
+d3.select("#george")
+    .on("click",function()
+       {
+        d3.select("#description")
+            .text("George Washington was also a revered General for the the American Colonists during the War. This image depicts Washington crossing the Delaware River to attack Enemy soldiers during the winter.")
+})
+
+d3.select("#constitutionimg")
+    .on("keydown", )
+
+
+
