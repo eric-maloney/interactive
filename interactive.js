050ca359d242242b91d3b6fef1ea28a6a25c1d2d
Eric M. Maloney <eric.maloney@centre.edu>
2020-03-04 20:03:14 -0500

    lab final

--- interactive.js
+++ interactive.js
@@ -18,11 +18,12 @@ d3.select("#george")
             .text("George Washington was also a revered General for the the American Colonists during the War. This image depicts Washington crossing the Delaware River to attack Enemy soldiers during the winter.")
 })
 
-/*
-var animation = d3.select("#constitutionimg")
-//animation.classed("run-animation", false);
-
-
+d3.select("#constitutionimg")
+    .on("click",function()
+       {
+        d3.select(".text")
+            .text("We the People of the United States, in Order to form a more perfect Union, establish Justice, insure domestic Tranquility, provide for the common defence, promote the general Welfare, and secure the Blessings of Liberty to ourselves and our Posterity, do ordain and establish this Constitution for the United States of America.")
+})
 
 
 
